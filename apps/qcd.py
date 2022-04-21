bde54622b49912be8ef5d519c39cee809b2854d0
kyokonii <103973290+kyokonii@users.noreply.github.com>
2022-04-21 19:11:40 +0900

    Update qcd.py

--- apps/qcd.py
+++ apps/qcd.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
-import plotly.express as px
-from plotly.subplots import make_subplots
-import datetime
 
 def app():
-  st.title('QCDの見える化')
+  st.title('QCDダッシュボード')
